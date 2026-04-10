784f38ebbe548e051c721c2b8f934c2b5704bdf0
Xitsa <xitsalarauko@googlemail.com>
2026-04-10 22:06:59 +0500

    Добавил тесты.

--- xParTests/UnitTest1.cs
+++ xParTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+﻿using xParLib;
+
 namespace xParTests;
 
 public class UnitTest1
@@ -6,5 +8,25 @@ public class UnitTest1
     public void Test1()
     {
         Assert.True(true);
+    }
+
+    [Theory]
+    [InlineData("Hello World", "Hello World[5, 5]")]
+    [InlineData("Test", "Test[4]")]
+    [InlineData("", "[0]")]
+    [InlineData("а б в г д е ё ж з и и́ кl 👨🏻‍❤️‍💋‍👨🏻 Ull", "а б в г д е ё ж з и и́ кl 👨🏻‍❤️‍💋‍👨🏻 Ull[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 3]")]
+    [InlineData("コ コ́", "コ コ́[2, 2]")]
+    public void Transform_ProcessesLines_Correctly(string input, string expected)
+    {
+        // Arrange
+        var transformer = new StringTransformer();
+        var inputLines = new[] { input };
+
+        // Act
+        var result = transformer.Transform(inputLines);
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal(expected, result[0]);
     }
 }
