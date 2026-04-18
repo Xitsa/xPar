1c78918372e369ad31f508032419acb7dd1ec9d8
Xitsa <xitsalarauko@googlemail.com>
2026-04-18 23:55:05 +0500

    Step 9

---
+++ xParLib/Reformat.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace xParLib
+{
+    /// <summary>
+    /// Реализация переноса логики форматирования. Функционал будет реализован в следующих шагах.
+    /// </summary>
+    public static class Reformat
+    {
+        /// <summary>
+        /// Переформатирует входные сегменты абзаца в отформатированный набор строк согласно
+        /// параметрам форматирования и вычисленным аффиксам IP.
+        /// Это сигнатура для будущей реализации: сегменты, диапазон начала/конца (включительно),
+        /// параметры форматирования и аффиксы.
+        /// </summary>
+        /// <param name="segments">Массив сегментов абзаца (LineSegment). Включает строки абзаца и их свойства.</param>
+        /// <param name="startIndex">Начальная позиция диапазона сегментов для обработки (включительно).</param>
+        /// <param name="endIndex">Конечная позиция диапазона сегментов для обработки (включительно).</param>
+        /// <param name="options">Параметры форматирования (Width, Just, Fit и др.).</param>
+        /// <param name="affixes">Результат вычисления префикса и суффикса IP (SetAffixesResult).</param>
+        /// <returns>Массив строк с отформатированным текстом абзаца.</returns>
+        public static string[] Reformat(
+            IReadOnlyList<LineSegment> segments,
+            int startIndex,
+            int endIndex,
+            ParOptions options,
+            SetAffixesResult affixes)
+        {
+            // TODO: реализовать логику форматирования на следующем этапе
+            return Array.Empty<string>();
+        }
+    }
+}
